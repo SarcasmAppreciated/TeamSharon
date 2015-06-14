d3a3d6840f571fcac6d4ee3a2743e755467997e1
Oliver Zhang <bestozhang@gmail.com>
2015-06-13 17:08:02 -0700

    New models

--- SuperheroesAndStuff/views.py
+++ SuperheroesAndStuff/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
 from django.http import JsonResponse
 from django.shortcuts import render
-import sqlite3
-
-def response(request):	
-    return JsonResponse({'foo':'LOL'})
+	
+def response(request):
+	data = {}
+	data = request.GET['name']
+	return HttpResponse(data)
 
 def index(request):
     return render(request, 'SuperheroesAndStuff/index.html')
