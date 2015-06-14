072f252494481fda0fa445f7b37b809a75acafd7
Oliver Zhang <bestozhang@gmail.com>
2015-06-14 16:50:32 -0700

    Commented out SQL commands for now

--- SuperheroesAndStuff/views.py
+++ SuperheroesAndStuff/views.py
@@ -40,19 +40,19 @@ fd.close()
 # all SQL commands (split on ';')
 sqlCommands = sqlFile.split(';')
 
-# Execute every command from the input file
-for command in sqlCommands:
-    # This will skip and report errors
-    # For example, if the tables do not yet exist, this will skip over
-    # the DROP TABLE commands
-    try:
-        connection.execute(command)
-    except OperationalError, msg:
-        print "Command skipped: ", msg
-
-# fetch all or one we'll go for all.
-results = cursor.fetchall()
-
-print "Hello"
+# # Execute every command from the input file
+# for command in sqlCommands:
+#     # This will skip and report errors
+#     # For example, if the tables do not yet exist, this will skip over
+#     # the DROP TABLE commands
+#     try:
+#         connection.execute(command)
+#     except OperationalError, msg:
+#         print "Command skipped: ", msg
+#
+# # fetch all or one we'll go for all.
+# results = cursor.fetchall()
+#
+# print "Hello"
 
 connection.close()
