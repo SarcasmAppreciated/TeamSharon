5420e31e359f4ea68c14a0639476858c3053c4cf
Oliver Zhang <bestozhang@gmail.com>
2015-06-13 17:06:33 -0700

    New models

--- SuperheroesAndStuff/views.py
+++ SuperheroesAndStuff/views.py
@@ -1,19 +1,20 @@
 from django.http import HttpResponse
 from django.http import JsonResponse
 from django.shortcuts import render
-	
+import sqlite3
+
 def response(request):	
-	return JsonResponse({'foo':'LOL'})
+    return JsonResponse({'foo':'LOL'})
 
 def index(request):
     return render(request, 'SuperheroesAndStuff/index.html')
-	
+
 # if __name__ == '__main__': 
 #	response()
 
 
 def query_to_string(arg1, arg2, arg3):
-    return "SELECT " + arg1 + "FROM " + arg2 + "WHERE " + arg3
+    return "SELECT * FROM SuperheroesAndStuff_kharacter"
 
 # the following code was adapted from the following source:
 # http://www.cdotson.com/2014/06/generating-json-documents-from-sqlite-databases-in-python/
@@ -32,5 +33,6 @@ cursor.execute(query_to_string("* ", "SuperheroesAndStuff_kharacter ", "charName
 # fetch all or one we'll go for all.
 results = cursor.fetchall()
 print results
+print "Hello"
 
 connection.close()
